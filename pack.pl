09fa997b270427137696172b3e4edf428bf14b9f
James N. V. Cash <james.nvc@gmail.com>
2018-04-28 13:10:19 -0400

    Bump version for new docs

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(css_write).
 title('DCG for generating CSS').
-version('0.0.1').
+version('0.0.2').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://github.com/jamesnvc/css_write').
 download('https://github.com/jamesnvc/css_write/releases/*.zip').
