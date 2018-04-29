c8c567a214dbf3a2a0977860a80a42bdd9ed1f23
James N. V. Cash <james.nvc@gmail.com>
2018-04-29 14:56:39 -0400

    Add requires to pack

--- pack.pl
+++ pack.pl
@@ -4,3 +4,4 @@ version('1.0.0').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://github.com/jamesnvc/css_write').
 download('https://github.com/jamesnvc/css_write/releases/*.zip').
+requires(list_util).
